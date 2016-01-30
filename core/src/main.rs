8d5909d101357f54c28e86748244730d254bf1ee
Matthew O'Connor <thegreendragon@gmail.com>
2016-01-30 00:10:28 -0700

    Begin writing parser for System Zero core.

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,5 +1,6 @@
 pub mod calc;
 pub mod ast; //::calc;
+mod parser;
 
 #[test]
 fn calculator4() {
@@ -9,17 +10,34 @@ fn calculator4() {
 
 #[cfg(not(test))]
 fn main() {
-  use ast::*;
+  // use ast::*;
+  //
+  // let codata = Const::Codata;
+  // println!("{:?}", codata);
+  // let a = Var::new("a", 0);
+  // let x = Var::new("x", 0);
+  // let expra = var(&a);
+  // let exprx = var(&x);
+  // println!("{:?}", x);
+  // let id = lam(a.clone(), constant(Const::Data), lam(x, expra, exprx));
+  // println!("{:?}", id);
+  // let apply_id = app(app(id, var(&Var::new("int", 0))), var(&Var::new("1", 0)));
+  // println!("{:?}", apply_id);
 
-  let codata = Const::Codata;
-  println!("{:?}", codata);
-  let a = Var::new("a", 0);
-  let x = Var::new("x", 0);
-  let expra = var(&a);
-  let exprx = var(&x);
-  println!("{:?}", x);
-  let id = lam(a.clone(), constant(Const::Data), lam(x, expra, exprx));
-  println!("{:?}", id);
-  let apply_id = app(app(id, var(&Var::new("int", 0))), var(&Var::new("1", 0)));
-  println!("{:?}", apply_id);
+  println!("{:?}", parser::parse_Expr("a").unwrap());
+  println!("{:?}", parser::parse_Expr("data").unwrap());
+  println!("{:?}", parser::parse_Expr("codata").unwrap());
+  println!("{:?}", parser::parse_Expr("\\ (a : data) -> a").unwrap());
+  println!("{:?}",
+           parser::parse_Expr("\\ (a : data) -> \\ (a : data) -> a").unwrap());
+  println!("{:?}",
+           parser::parse_Expr("forall (a : data) -> a").unwrap());
+  println!("{:?}",
+           parser::parse_Expr("forall (a : data) -> forall (b : data) -> a")
+             .unwrap());
+  println!("{:?}",
+           parser::parse_Expr("forall (a : data) -> \\ (a : data) -> a")
+             .unwrap());
+  println!("{:?}", parser::parse_Expr("a -> a").unwrap());
+  // println!("{:?}", parser::parse_Expr("forall (a : data) -> a -> a").unwrap());
 }
