a7b1804b4d969d9ae53bc7a7bdb70c049b3d9cea
Matthew O'Connor <thegreendragon@gmail.com>
2016-02-03 22:58:02 -0700

    Clean up szi to use lines() rather than read_line(). Write the beginnings of bool.sz - hopefully to provide an implementation of bools. Not sure where to put the file yet.

--- szi/src/main.rs
+++ szi/src/main.rs
@@ -2,28 +2,31 @@
 extern crate system_zero_core;
 
 use std::io;
-use std::io::Write;
+use std::io::{BufRead, Write};
 use system_zero_core::*;
 use system_zero_core::ast::*;
 
 const PROMPT: &'static str = "> ";
 
+fn prompt() -> () {
+  print!("{}", PROMPT);
+  io::stdout().flush().unwrap()
+}
+
 fn repl() -> io::Result<()> {
-  loop {
-    let mut input = String::new();
-    print!("{}", PROMPT);
-    try!(io::stdout().flush());
-    match io::stdin().read_line(&mut input) {
-      Ok(_) => {
-        let line: &str = &input[..];
-        match parse_one(line) {
-          Ok(one) => print!("{:?}", one.normalize()),
-          Err(error) => println!("Couldn't parse: {:?}", error),
-        }
+  prompt();
+  let stdin = io::stdin();
+  for line in stdin.lock().lines() {
+    let line = line.unwrap();
+    if line.len() > 0 {
+      match parse_one(&line[..]) {
+        Ok(one) => print!("{:?}", one.normalize()),
+        Err(error) => println!("Couldn't parse: {:?}", error),
       }
-      Err(error) => println!("Couldn't read: {}", error),
     }
+    prompt();
   }
+  Ok(())
 }
 
 fn main() {
