565357b33391bd561c831d25806cb33fa9bf48e8
Matthew O'Connor <thegreendragon@gmail.com>
2016-02-12 21:20:36 -0700

    Implement Debug for Env. Begin implementing environment aware normalizing.

--- szi/src/main.rs
+++ szi/src/main.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2016, Matthew O'Connor
 extern crate system_zero_core;
 
+use std::fmt::{Debug, Formatter};
 use std::io;
 use std::io::{BufRead, Write};
 use system_zero_core::*;
@@ -13,26 +14,35 @@ fn prompt() -> () {
   io::stdout().flush().unwrap()
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Eq, PartialEq)]
 struct Env(Vec<Def>);
 
-impl IntoIterator for Env {
-    type Item = Def;
-    type IntoIter = ::std::vec::IntoIter<Def>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
+impl Debug for Env {
+  fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
+    for def in &self.0 {
+      try!(writeln!(fmt, "{:?}", def));
     }
+    Ok(())
+  }
 }
+
+// impl IntoIterator for Env {
+//     type Item = Def;
+//     type IntoIter = ::std::vec::IntoIter<Def>;
+
+//     fn into_iter(self) -> Self::IntoIter {
+//         self.0.into_iter()
+//     }
+// }
 
 impl Env {
   fn new() -> Env {
     Env(Vec::new())
   }
 
-  fn get_val(self, var: &Var) -> Option<Def> {
-    self.into_iter().rev().find(|&ref def| {
-      match *def {
+  fn get_val(&self, var: &Var) -> Option<&Def> {
+    self.0.iter().rev().find(|def| {
+      match **def {
         Def::Val(ref v, _) => *v == *var,
         Def::Ty(_, _) => false,
       }
@@ -41,6 +51,56 @@ impl Env {
 
   fn push(&mut self, def: Def) {
     self.0.push(def)
+  }
+
+  fn pop(&mut self) {
+    self.0.pop();
+  }
+}
+
+fn normalize_in(e: &Expr, env: &mut Env) -> Expr {
+  match *e {
+    Expr::Const(ref constant) => Expr::Const(constant.clone()),
+    Expr::Var(ref var) => {
+      match env.get_val(var) {
+        Some(ref def) => def.expr().clone(),
+        None => Expr::Var(var.clone()),
+      }
+    }
+    Expr::Lam(ref var, ref ty, ref body) => {
+      env.push(Def::Val(var.clone(), Expr::Var(var.clone())));
+      let ty = normalize_in(ty, env);
+      let body = normalize_in(body, env);
+      let lam = Expr::Lam(var.clone(), Box::new(ty), Box::new(body));
+      env.pop();
+      lam
+    }
+    Expr::Pi(ref var, ref ty, ref body) => {
+      env.push(Def::Ty(var.clone(), Expr::Var(var.clone())));
+      let ty = normalize_in(ty, env);
+      let body = normalize_in(body, env);
+      let pi = Expr::Pi(var.clone(), Box::new(ty), Box::new(body));
+      env.pop();
+      pi
+    }
+    Expr::App(ref f, ref arg) => {
+      let f = f.normalize();
+      // let f = *f.clone();
+      let arg = arg.normalize();
+      Expr::App(Box::new(f), Box::new(arg))
+      // if let Expr::Lam(var, _, body) = f {
+      // let lam = replace(&var, &arg, &body).normalize();
+      // println!("normalized = {}", lam.to_string());
+      // lam
+      // } else if let Expr::Pi(var, _, body) = f {
+      // let pi = replace(&var, &arg, &body).normalize();
+      // println!("normalized = {}", pi.to_string());
+      // pi
+      // } else {
+      // panic!("f isn't a function {}", f.to_string())
+      // Expr::App(Box::new(f), Box::new(arg))
+      // }
+    }
   }
 }
 
@@ -52,7 +112,7 @@ fn repl() -> io::Result<()> {
   for line in stdin.lock().lines() {
     let line = line.unwrap();
     if line == ":env" {
-      println!("{:?}", env);
+      print!("{:?}", env);
     } else if line == ":history" {
       println!("{:?}", history);
     } else if line == ":quit" {
@@ -66,7 +126,9 @@ fn repl() -> io::Result<()> {
               print!("{:?}", one);
               env.push(def.clone());
             }
-            One::Expr(_) => print!("{:?}", one.normalize()),
+            One::Expr(ref e) => {
+              print!("{:?}", One::Expr(normalize_in(e, &mut env)))
+            }
           }
         }
         Err(error) => println!("Couldn't parse: {:?}", error),
