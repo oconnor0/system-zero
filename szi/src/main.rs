3efa2174bcd0a70b79e77fc87129ce69f799b671
Matthew O'Connor <thegreendragon@gmail.com>
2016-02-15 13:31:27 -0700

    Split REPL into multiple functions - one for eval and one for REPL commands.

--- szi/src/main.rs
+++ szi/src/main.rs
@@ -23,6 +23,58 @@ fn prompt() -> () {
   io::stdout().flush().unwrap()
 }
 
+fn support(line: &String, mut env: &mut Env, history: &Vec<String>) {
+  if line == ":env" {
+    print!("{:?}", env);
+  } else if line == ":history" {
+    println!("{:?}", history);
+  } else if line == ":quit" || line == ":exit" {
+    std::process::exit(1);
+  } else if line == ":clear" {
+    env.clear();
+    load_prelude(&mut env);
+  } else if line.starts_with(":ty ") {
+    let name = &line[4..];
+    match env.get_ty(&Var::new(name, 0)) {
+      Some(ref def) => println!("{:?}", def),
+      None => println!("No type for `{}`", name),
+    }
+  } else if line.starts_with(":val ") {
+    let name = &line[5..];
+    match env.get_val(&Var::new(name, 0)) {
+      Some(ref def) => println!("{:?}", def),
+      None => println!("No value for `{}`", name),
+    }
+  } else if line.starts_with(":def ") {
+    let name = &line[5..];
+    match env.get_ty(&Var::new(name, 0)) {
+      Some(ref def) => println!("{:?}", def),
+      None => println!("No type for `{}`", name),
+    }
+    match env.get_val(&Var::new(name, 0)) {
+      Some(ref def) => println!("{:?}", def),
+      None => println!("No value for `{}`", name),
+    }
+  }
+}
+
+fn eval<'input>(line: &'input str, mut env: &mut Env) -> Result<'input, One> {
+  match parse_one(&line) {
+    Ok(one) => {
+      match one {
+        One::Def(ref def) => {
+          let def = def.normalize_in(&mut env);
+          let one = One::Def(def.clone());
+          env.push(def);
+          Ok(one)
+        }
+        One::Expr(ref e) => Ok(One::Expr(e.normalize_in(&mut env))),
+      }
+    }
+    Err(error) => Err(error),
+  }
+}
+
 fn repl() -> io::Result<()> {
   let mut history: Vec<String> = vec![];
   let mut env = Env::new();
@@ -31,33 +83,14 @@ fn repl() -> io::Result<()> {
   let stdin = io::stdin();
   for line in stdin.lock().lines() {
     let line = line.unwrap();
-    if line == ":env" {
-      print!("{:?}", env);
-    } else if line == ":history" {
-      println!("{:?}", history);
-    } else if line == ":quit" || line == ":exit" {
-      break;
-    } else if line == ":clear" {
-      env.clear();
-      load_prelude(&mut env);
-    } else if line.starts_with("--") {
+    history.push(line.clone());
+    if line.starts_with("--") {
       ()
+    } else if line.starts_with(":") {
+      support(&line, &mut env, &history);
     } else if line.len() > 0 {
-      history.push(line.clone());
-      match parse_one(&line[..]) {
-        Ok(one) => {
-          match one {
-            One::Def(ref def) => {
-              let def = def.normalize_in(&mut env);
-              let one = One::Def(def.clone());
-              print!("{:?}", one);
-              env.push(def);
-            }
-            One::Expr(ref e) => {
-              print!("{:?}", One::Expr(e.normalize_in(&mut env)))
-            }
-          }
-        }
+      match eval(&line[..], &mut env) {
+        Ok(one) => print!("{:?}", one),
         Err(error) => println!("Couldn't parse: {:?}", error),
       }
     }
@@ -69,6 +102,5 @@ fn repl() -> io::Result<()> {
 fn main() {
   println!("szi - system zero interpreter");
   println!("Copyright (c) 2016, Matthew O'Connor <thegreendragon@gmail.com>");
-  // Return 0 if repl returned Ok.
-  std::process::exit(!repl().is_ok() as i32)
+  repl().unwrap()
 }
